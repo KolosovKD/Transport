c9fca00827c25bbe4d5f5b3cedafd11bce17250f
kolosov <spikecyrill@gmail.com>
2020-04-13 21:21:55 +0300

    add comment

--- src/ru/avalon/Transport/Application.java
+++ src/ru/avalon/Transport/Application.java
@@ -2,6 +2,9 @@ package ru.avalon.Transport;
 
 import ru.avalon.Transport.transport.*;
 
+/**
+ * Комментарий описывающий назначение класса
+ */
 public class Application {
 
     public static void main(String[] args) {
